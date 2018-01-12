566cc9573ad39ffe806019d01a488ca22bf0d65f
Hong Jianwei <hjw3.9@foxmail.com>
2018-01-12 10:57:52 +0800

    base64page 配置路由；

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -3,16 +3,24 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+const Base64page = resolve => require(['@/components/base64page/index.vue'], resolve)
+
+
+
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
+    // {
+    //   path: '/',
+    //   name: 'landing-page',
+    //   component: require('@/components/LandingPage').default
+    // },
     {
       path: '*',
       redirect: '/'
+    },{
+      path:'/',
+      name:'base64-page',
+      component:Base64page
     }
   ]
 })
