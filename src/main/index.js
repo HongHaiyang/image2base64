6627116e2b3e3f53ba962c2bbcc708eac9d93377
Hong Jianwei <hjw3.9@foxmail.com>
2018-01-14 23:03:28 +0800

    添加打包后的应用快捷键支持；

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron'
+const {Menu} = require('electron')
 
 /**
  * Set `__static` path to static files in production
@@ -8,12 +9,32 @@ if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
+var template = [{
+  label: "Application",
+  submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+  ]}, {
+  label: "Edit",
+  submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+  ]}
+];
+
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
 function createWindow () {
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
   /**
    * Initial window options
    */
